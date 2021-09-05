c6847e9b5e137d58503bf37a2a3643de5eb1e20c
kimkyury <mandarining0918@gmail.com>
2021-09-05 20:36:54 +0900

    init

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.db.models.expressions import Value
+from core import models as core_models
 
-# Create your models here.
+
+class Review(core_models.TimeStampedModel):
+    """Review Model Definition"""
+
+    review = models.TextField()
+    accuracy = models.IntegerField()
+    communication = models.IntegerField()
+    cleanliness = models.IntegerField()
+    location = models.IntegerField()
+    check_in = models.IntegerField()
+    value = models.IntegerField()
+    user = models.ForeignKey(
+        "users.User", related_name="reviews", on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return f"{self.review}"
+
+    def rating_average(self):
+        avg = (
+            self.accuracy
+            + self.communication
+            + self.cleanliness
+            + self.location
+            + self.check_in
+            + self.value
+        ) / 6
+        return round(avg, 2)  # 반올림값
--- users/models.py
+++ users/models.py
@@ -1,11 +1,15 @@
 from django.db import models
-
 # Create your models here.
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
-class User(AbstractUser):
+
+
+
+
+
+class user(AbstractUser):
 
     """Custom User Model"""
 
