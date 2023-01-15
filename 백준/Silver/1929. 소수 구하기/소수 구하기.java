914327d561123fe5b25290e5450884cfc3e76d37
KGYURY <71677810+kimkyury@users.noreply.github.com>
2023-01-15 22:51:52 +0900

    [Silver III] Title: 소수 구하기, Time: 292 ms, Memory: 27540 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.java
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -1,41 +1,29 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main {
 
-  public static void solve(int N, int M) {
+  public static void solve(int N, int M) throws IOException {
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     // over N  ~ under M
-    if (N == 1 && M == 1) {
-      // System.out.println(""); // 1은 소수 아니잖아
-      return;
-    } else if (M == 2) {
-      System.out.println("2");
-      return;
-    }
-
     int[] candidate = new int[M + 1];
-    // avaliable -> 0, UnAvailable -> 1
-    candidate[1] = 1;
-
-    // 완전탐색 X
-    // 에라토스테네스의 체를 써보자 -> 반복되는 배수들 모두 제거
-    // if (N == 1) N = 2; // 상향조정
-    // int num = 2;
     for (int i = 2; i <= M; i++) {
-      if (candidate[i] == 0) {
-        for (int j = i + i; j <= M; j += i) {
-          candidate[j] = 1;
-        }
+      if (candidate[i] == 1) continue;
+
+      // 최초의 출력 이후 Block될 예정
+      if (i >= N) bw.write(String.valueOf(i) + '\n');
+      // if (i >= N) System.out.println(i);
+
+      for (int j = i + i; j <= M; j += i) {
+        candidate[j] = 1;
       }
     }
 
-    for (int i = N; i <= M; i++) {
-      if (candidate[i] == 0) {
-        System.out.println(i);
-      }
-    }
+    bw.flush();
   }
 
   public static void main(String[] args) throws IOException {
