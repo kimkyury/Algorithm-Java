cd779bb0c22c55af4cb678ca5c33595bc8488a35
KGYURY <71677810+kimkyury@users.noreply.github.com>
2023-01-27 23:10:32 +0900

    [Silver IV] Title: 괄호, Time: 132 ms, Memory: 14436 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -1,44 +1,50 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
 import java.util.Stack;
+import java.util.StringTokenizer;
 
-public class Main {
+class Main {
 
-	public static void main(String[] args) throws Exception{
-		// TODO Auto-generated method stub
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int T = Integer.parseInt(br.readLine());
-		char c =' ';
-		Stack<Character> stack = new Stack<>();
-		
-		for (int tc = 0; tc < T; tc++) {
-			String s = br.readLine();
-			stack.clear();
-			
-			for (int i = 0; i < s.length(); i++) {
-				c = s.charAt(i);
-				
-				// '('는 stack에 넣기
-				if(c =='(') {
-					stack.add(c);
-				}
-				else if(c == ')') {
-					if(stack.empty()) {
-						stack.add(c);
-						break;
-					}
-					stack.pop();
-				}
-			}
-			if(stack.empty()) {
-				System.out.println("YES");
-			}
-			else {
-				System.out.println("NO");
-			}
-			
-		}
-		
-	}
+  static BufferedWriter bw = new BufferedWriter(
+    new OutputStreamWriter(System.out)
+  );
 
+  public static String solve(String str) {
+    Stack<Character> stack = new Stack<>();
+
+    for (char c : str.toCharArray()) {
+      if (c == '(') {
+        stack.push(c);
+      } else {
+        if (stack.isEmpty()) {
+          return "NO";
+        }
+        stack.pop();
+      }
+    }
+
+    if (!stack.isEmpty()) {
+      return "NO";
+    }
+
+    return "YES";
+  }
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    int n = Integer.parseInt(br.readLine());
+
+    for (int i = 0; i < n; i++) {
+      String str = br.readLine();
+
+      bw.write(solve(str) + '\n');
+    }
+
+    bw.flush();
+  }
 }
