0347c396456b6f27ad0da3786478693aac281e6c
KGYURY <71677810+kimkyury@users.noreply.github.com>
2023-01-27 23:57:44 +0900

    [Silver IV] Title: 카드2, Time: 188 ms, Memory: 53336 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -1,27 +1,40 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
-public class Main {
+class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in);
-		Queue<Integer> q = new LinkedList<>();
-		int n = sc.nextInt();
-		for(int i = 0 ;i<n;i++) {
-			q.add(i+1);
-		}
-		while(true) {
-			if(q.size() == 1) {
-				break;
-			}
-			q.poll();
-			q.add(q.poll());
-		}
-		System.out.println(q.poll());
-		
-		
-	}
+  static BufferedWriter bw = new BufferedWriter(
+    new OutputStreamWriter(System.out)
+  );
 
+  public static int solve(int N) {
+    Queue<Integer> q = new LinkedList<>();
+    for (int i = 1; i <= N; i++) {
+      q.offer(i);
+    }
+
+    while (q.size() != 1) {
+      q.poll(); // 삭제
+      int tmp = q.poll();
+      q.offer(tmp); // 재삽입
+    }
+
+    return q.poll();
+  }
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    // StringTokenizer st = new StringTokenizer(br.readLine());
+    int N = Integer.parseInt(br.readLine());
+
+    bw.write(String.valueOf(solve(N)));
+
+    bw.flush();
+  }
 }
