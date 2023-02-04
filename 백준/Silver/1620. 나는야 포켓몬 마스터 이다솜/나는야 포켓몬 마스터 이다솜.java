1ac8ff3b233ca90d30cf1c26190e0fad3df4ab20
KGYURY <71677810+kimkyury@users.noreply.github.com>
2023-02-04 13:19:27 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 604 ms, Memory: 55228 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
@@ -1,46 +1,56 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.Scanner;
 import java.util.StringTokenizer;
+import java.util.TreeMap;
 
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in);
-		int m = sc.nextInt();
-		int n = sc.nextInt();
-		
-		Map<String,Integer> map_S = new LinkedHashMap<>();
-		Map<Integer,String> map_I = new LinkedHashMap<>();
-		
-		for(int i = 0; i<m;i++) {
-			String s = sc.next();
-			map_S.put(s, i);
-			map_I.put(i, s);
-		}
-		
-		for(int i = 0; i<n;i++) {
-			String s = sc.next();
-			StringTokenizer st = new StringTokenizer(s);
-			if(isInteger(s)) {
-				int a = Integer.parseInt(s);
-				System.out.println(map_I.get(a-1));
-			}
-			else {
-				System.out.println(map_S.get(s)+1);
-			}
-		}
-	}
-	
-	public static boolean isInteger(String s) {
-		try {
-			Integer.parseInt(s);
-			return true;
-		}catch(NumberFormatException ex){
-			return false;
-		}
-	}
+  static HashMap<String, Integer> strDic;
+  static HashMap<Integer, String> indexDic;
+  // static HashSet<String> S;
+
+  static BufferedWriter bw = new BufferedWriter(
+    new OutputStreamWriter(System.out)
+  );
+
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st;
+
+    // for 시간단축
+    strDic = new HashMap<>();
+    indexDic = new HashMap<>();
+
+    st = new StringTokenizer(br.readLine());
+    int N = Integer.parseInt(st.nextToken());
+    int M = Integer.parseInt(st.nextToken());
+
+    for (int i = 1; i <= N; i++) {
+      String str = br.readLine();
+      strDic.put(str, i);
+      indexDic.put(i, str);
+    }
+
+    for (int i = 0; i < M; i++) {
+      String input = br.readLine();
+
+      // if 숫자라면
+      if (input.charAt(0) >= 65) {
+        bw.write(strDic.get(input) + "\n");
+      } else {
+        int index = Integer.parseInt(input);
+        bw.write(indexDic.get(index) + "\n");
+      }
+    }
+
+    // bw.write(String.valueOf(cnt));
+    bw.flush();
+  }
+  // pq
+  // 1 4 5 -> poll:2 (3/2 +1)
+  // 1 2 3 4 5 -> poll: 3 (5/2+1)
 
 }
