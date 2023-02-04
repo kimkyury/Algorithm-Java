00f315984214a25d78d4fe9d93c8769ba32f9352
KGYURY <71677810+kimkyury@users.noreply.github.com>
2023-02-04 12:54:35 +0900

    [Silver III] Title: 문자열 집합, Time: 412 ms, Memory: 31236 KB -BaekjoonHub

--- 백준/Silver/14425. 문자열 집합/문자열 집합.java
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.java
@@ -1,30 +1,48 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.HashSet;
-import java.util.Set;
 import java.util.StringTokenizer;
 
-public class Main{
+public class Main {
 
-	public static void main(String[] args) throws Exception {
-		// TODO Auto-generated method stub
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int n = Integer.parseInt(st.nextToken());
-		int m = Integer.parseInt(st.nextToken());
-		Set<String> arr = new HashSet<>();
-		
-		for(int i = 0;i<n;i++) {
-			arr.add(br.readLine());
-		}
-		int result = 0;
-		for(int i = 0;i<m;i++) {
-			String s = br.readLine();
-			if(arr.contains(s)) {
-				result++;
-			}
-		}
-		System.out.println(result);
-	}
+  // static HashMap<String, Integer> S;
+  static HashSet<String> S;
+
+  static BufferedWriter bw = new BufferedWriter(
+    new OutputStreamWriter(System.out)
+  );
+
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st;
+    S = new HashSet<>();
+
+    st = new StringTokenizer(br.readLine());
+    int N = Integer.parseInt(st.nextToken());
+    int M = Integer.parseInt(st.nextToken());
+
+    for (int i = 0; i < N; i++) {
+      String str = br.readLine();
+      // S.put(str, S.getOrDefault(S.get(str) + 1, 0));
+      S.add(str);
+    }
+
+    int cnt = 0;
+    for (int i = 0; i < M; i++) {
+      String str = br.readLine();
+
+      if (S.contains(str)) {
+        cnt++;
+      }
+    }
+
+    bw.write(String.valueOf(cnt));
+    bw.flush();
+  }
+  // pq
+  // 1 4 5 -> poll:2 (3/2 +1)
+  // 1 2 3 4 5 -> poll: 3 (5/2+1)
 
 }
