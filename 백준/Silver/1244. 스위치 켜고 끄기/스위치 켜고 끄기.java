4a7daaee4dc7e78976b3c690a28fc25edc2c5971
KGYURY <71677810+kimkyury@users.noreply.github.com>
2023-02-07 17:56:28 +0900

    [Silver IV] Title: 스위치 켜고 끄기, Time: 132 ms, Memory: 14272 KB -BaekjoonHub

--- 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.java
+++ 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.java
@@ -1,74 +1,123 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
-public class Main {
+class Student {
 
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		// TODO Auto-generated method stub
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int s_num = Integer.parseInt(br.readLine());
-		
-		int[] swt = new int[s_num];
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		for(int i=0;i<s_num;i++) {
-			swt[i]=Integer.parseInt(st.nextToken());
-		}
-		int student_n = Integer.parseInt(br.readLine());
-		
-		for(int i=0;i<student_n;i++) {
-			StringTokenizer st1 = new StringTokenizer(br.readLine());
-			if(Integer.parseInt(st1.nextToken())==1) swt=man(swt,Integer.parseInt(st1.nextToken())); //남학생
-			else swt=woman(swt,Integer.parseInt(st1.nextToken())); //여학생
-			
-		}
-		
-		for (int i=0; i<swt.length;i++) {
-			if(i%20==0 && i!=0) System.out.println();
-			System.out.print(swt[i]+" ");
-			
-		}
-		
-		
+	int gender;
+	int number;
+
+	Student(int gender, int number) {
+		this.gender = gender;
+		this.number = number;
 	}
-	
-	static int[] man(int[] swt,int n) { //배수면 토글
-		
-		int i=1;
-		int temp=n;
-		while(n-1<swt.length) {
-			
-			if(swt[n-1]==1) swt[n-1]=0;
-			else swt[n-1]=1;
-			i+=1;
-			n=(temp)*i;
-		}
-		return swt;
-	}
-	
-	static int[] woman(int[] swt,int n) { //양옆이 같으면 토글
-		n-=1;
-		if(swt[n]==1) swt[n]=0;
-		else swt[n]=1;
-		
-		int i=1;
-		while(n-i>=0 && n+i<swt.length) {
-			if(swt[n-i]!=swt[n+i]) break;
-			else {
-				if(swt[n-i]==1) {
-					swt[n-i]=0;
-					swt[n+i]=0;
+}
+
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+	static int N;
+
+	public static int[] solve(int[] switchs, Student[] students) throws IOException {
+
+		for (Student student : students) {
+			int gender = student.gender;
+			int number = student.number;
+
+			if (gender == 1) {
+				for (int i = 1; i <= N; i++) {
+					if (i % number == 0) {
+
+						if (switchs[i - 1] == 0)
+							switchs[i - 1] = 1;
+						else
+							switchs[i - 1] = 0;
+					}
 				}
-				else {
-					swt[n-i]=1;
-					swt[n+i]=1;
+
+			} else if (gender == 2) {
+				number--; // 인덱스 맞추기
+				// number을 기준으로 좌우 탐색
+				int left = number - 1;
+				int right = number + 1;
+
+				if (switchs[number] == 0)
+					switchs[number] = 1;
+				else
+					switchs[number] = 0;
+
+				while (left >= 0 && right <= N - 1) {
+					if (switchs[left] == switchs[right]) {
+						// left
+						if (switchs[left] == 0)
+							switchs[left] = 1;
+						else
+							switchs[left] = 0;
+
+						// right
+						if (switchs[right] == 0)
+							switchs[right] = 1;
+						else
+							switchs[right] = 0;
+
+						left--;
+						right++;
+					} else
+						break;
 				}
 			}
-			i+=1;
 		}
-		return swt;
+		return switchs;
 	}
 
+	public static void main(String[] args) throws IOException {
+
+//		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		// 스위치는 커져있거나 꺼져있다
+		// boy : (스위치번호 % 본인 수 ) == 0 -> 스위치 상태 바꾸기
+		// girl : 좌우대칭이 끝나는 지점까지 찾아가기, 그 후 해당 구간의 스위치 상태 모두 반전
+		// 단, 이 구간에 속한 스위치 개수는 항상 홀수가 될 것임
+
+		N = Integer.parseInt(br.readLine());
+
+		int[] switchs = new int[N];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			if (st.nextToken().equals("0"))
+				switchs[i] = 0;
+			else
+				switchs[i] = 1;
+		}
+
+		int cntStudent = Integer.parseInt(br.readLine());
+		Student[] students = new Student[cntStudent];
+
+		for (int i = 0; i < cntStudent; i++) {
+			st = new StringTokenizer(br.readLine());
+			int gender = Integer.parseInt(st.nextToken());
+			int number = Integer.parseInt(st.nextToken());
+			Student student = new Student(gender, number);
+			students[i] = student;
+		}
+
+		int[] results = solve(switchs, students);
+//		System.out.println(results.toString());
+
+		int cnt = 0;
+		for (int result : results) {
+			cnt++;
+			bw.write(String.valueOf(result) + " ");
+			if (cnt % 20 == 0) {
+				bw.write("\n");
+			}
+		}
+
+		bw.flush();
+
+	}
 }
