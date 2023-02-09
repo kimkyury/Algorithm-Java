b324429a7b807ec90b5fc0387b32755c62edcac4
KGYURY <71677810+kimkyury@users.noreply.github.com>
2023-02-09 16:23:50 +0900

    [Silver II] Title: 도영이가 만든 맛있는 음식, Time: 124 ms, Memory: 14140 KB -BaekjoonHub

--- 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.java
+++ 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.java
@@ -24,51 +24,44 @@ public class Main {
 	static int minDiff;
 	static Taste[] tastes;
 
-	public static void solve(int sideIdx, int idx, int totalBitter, int totalSour) {
-
-		if (sideIdx == N) {
-			if (idx == 0)
-				return;
-//			System.out.println(sideIdx + " " + idx + " " + totalBitter + " " + totalSour);
-//			System.out.println("어?");
-			minDiff = Math.min(minDiff, Math.abs(totalBitter - totalSour));
-			return;
-		}
-
-		Taste tmpT = tastes[sideIdx];
-		solve(sideIdx + 1, idx + 1, totalBitter * tmpT.bitter, totalSour + tmpT.sour);
-		solve(sideIdx + 1, idx, totalBitter, totalSour);
-
-	}
-
 	public static void main(String[] args) throws IOException {
-
-		// 음식의 신 맛 = 신맛의 곱
-		// 음식의 쓴맛 = 신맛의 합
-		// abs[신맛의 곱 - 신맛의 합] 가장 작은 요리 찾기
 
 		N = Integer.parseInt(br.readLine());
 		tastes = new Taste[N];
 		minDiff = Integer.MAX_VALUE;
 
+		// 비트마스크를 사용하기
+		// 음식으로 만들 수 있는 모든 조합의 수를 만들자
+
+		int subsetCnt = 1 << N;
+
+		StringTokenizer st;
 		for (int i = 0; i < N; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			int sourCnt = Integer.parseInt(st.nextToken());
-			int bitterCnt = Integer.parseInt(st.nextToken());
-			tastes[i] = new Taste(sourCnt, bitterCnt);
+			st = new StringTokenizer(br.readLine());
+			int bitter = Integer.parseInt(st.nextToken());
+			int sour = Integer.parseInt(st.nextToken());
+			tastes[i] = new Taste(bitter, sour);
 		}
 
-		if (N == 1) {
-			minDiff = Math.abs(tastes[0].sour - tastes[0].bitter);
-			bw.write(String.valueOf(minDiff));
-			bw.flush();
-			return;
+		Taste tmpT;
+
+		int min = Integer.MAX_VALUE;
+		for (int i = 1; i < subsetCnt; i++) { // 여기 0 제외처리
+			int totalBitter = 1;
+			int totalSour = 0;
+			for (int j = 0; j < N; j++) {
+				if ((i & (1 << j)) != 0) {
+
+					tmpT = tastes[j];
+					totalBitter *= tmpT.bitter;
+					totalSour += tmpT.sour;
+
+				}
+			}
+			min = Math.min(min, Math.abs(totalBitter - totalSour));
 		}
 
-		// 조합을 구해야한다
-		solve(0, 0, 1, 0);
-
-		bw.write(String.valueOf(minDiff));
+		bw.write(String.valueOf(min));
 
 		//
 		bw.flush();
