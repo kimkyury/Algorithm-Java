406c525d69efe584293379131fc714818e00452e
KGYURY <71677810+kimkyury@users.noreply.github.com>
2023-02-10 17:10:38 +0900

    [Silver IV] Title: 카드2, Time: 200 ms, Memory: 29012 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -1,40 +1,34 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.LinkedList;
+import java.util.ArrayDeque;
 import java.util.Queue;
-import java.util.StringTokenizer;
 
-class Main {
+public class Main {
 
-  static BufferedWriter bw = new BufferedWriter(
-    new OutputStreamWriter(System.out)
-  );
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		Queue<Integer> q = new ArrayDeque<>();
+		
+		for(int i=1;i<N+1;i++) {
+			q.offer(i);
+		}
+		int temp;
+		while(true) {
+			if(q.size()==1) {
+				System.out.println(q.poll());
+				break;
+			}
+			if(q.size()>=2) {
+				q.poll();
+				q.offer(q.poll());
+			}
+		}
 
-  public static int solve(int N) {
-    Queue<Integer> q = new LinkedList<>();
-    for (int i = 1; i <= N; i++) {
-      q.offer(i);
-    }
+	}
 
-    while (q.size() != 1) {
-      q.poll(); // 삭제
-      int tmp = q.poll();
-      q.offer(tmp); // 재삽입
-    }
-
-    return q.poll();
-  }
-
-  public static void main(String[] args) throws IOException {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    // StringTokenizer st = new StringTokenizer(br.readLine());
-    int N = Integer.parseInt(br.readLine());
-
-    bw.write(String.valueOf(solve(N)));
-
-    bw.flush();
-  }
 }
