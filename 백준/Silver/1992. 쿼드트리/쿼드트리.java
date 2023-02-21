770500f2a1e8314345b360bbac3c2709bd995837
KGYURY <71677810+kimkyury@users.noreply.github.com>
2023-02-21 10:53:42 +0900

    [Silver I] Title: 쿼드트리, Time: 124 ms, Memory: 14260 KB -BaekjoonHub

--- 백준/Silver/1992. 쿼드트리/쿼드트리.java
+++ 백준/Silver/1992. 쿼드트리/쿼드트리.java
@@ -1,99 +1,83 @@
+
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static StringTokenizer st;
 
-  public static String solve(int N, int[][] map) {
-    // 1. 4분할시키기
-    // 2. 각 분할된 곳에서 1과, 0으로만 되어있는지 체킹
-    // 3. 아니라면 다시 4분할
+	static int N;
+	static int[][] quad;
+	static StringBuilder sb = new StringBuilder();
 
-    // 2사분면(0,  0) ~ (2/n, 2/n)
-    // 1사분면 (0, 2/n) ~ (2/n, n)
-    // 3사분면 (2/n, 0) ~ (n, 2/n)
-    // 4사분면 ( 2/n, 2/n) ~ (n, n)
+	public static void solve(int index, int cnt) {
 
-    boolean flag = true;
-    int tmp = map[0][0];
-    for (int[] row : map) {
-      for (int num : row) {
-        if (tmp != num) {
-          flag = false;
-          break;
-        }
-      }
-      if (!flag) break;
-    }
-    if (flag) return tmp + "";
+	}
 
-    int[][] quadrants = {
-      { 0, 0, N / 2, N / 2 }, // (y1, x1) (y2, x2)
-      { 0, N / 2, N / 2, N },
-      { N / 2, 0, N, N / 2 },
-      { N / 2, N / 2, N, N },
-    };
+	public static void check(int sy, int sx, int ey, int ex) {
+		// x : sx, (sx+ex)/2, ex
+		// y : sy, (sy+ey)/2, ey
+		int initV = quad[sy][sx];
+		if (ey - sy == 1) {
+			sb.append(initV);
+			return;
+		}
 
-    String result = "(";
-    int[][] newMap = new int[N / 2][N / 2];
-    for (int[] quadrant : quadrants) {
-      // isPerpect = true;
-      int startY = quadrant[0];
-      int startX = quadrant[1];
-      int endY = quadrant[2];
-      int endX = quadrant[3];
+		boolean isFullFill = true;
 
-      // 사분면 잘라내기
-      int idxNewMapI = 0;
-      int idxNewMapJ;
-      for (int i = startY; i < endY; i++) {
-        idxNewMapJ = 0;
-        for (int j = startX; j < endX; j++) {
-          newMap[idxNewMapI][idxNewMapJ] = map[i][j];
-          idxNewMapJ++;
-        }
-        idxNewMapI++;
-      }
+		for (int i = sy; i < ey; i++) {
+			for (int j = sx; j < ex; j++) {
+				if (quad[i][j] != initV)
+					isFullFill = false;
 
-      // 해당 영역이 통일되어있는지 확인
-      int startNum = newMap[0][0];
-      // System.out.println(startNum);
-      boolean isAllSame = confirmIsAllSame(newMap, startNum);
-      if (isAllSame) {
-        result += startNum;
-      } else {
-        result += solve(N / 2, newMap);
-      }
-    }
+			}
+			if (!isFullFill) {
+				break;
+			}
+		}
 
-    return result + ")";
-  }
+		if (!isFullFill) {
+			int my = (sy + ey) / 2;
+			int mx = (sx + ex) / 2;
+			sb.append("(");
+			check(sy, sx, my, mx);
+			check(sy, mx, my, ex);
+			check(my, sx, ey, mx);
+			check(my, mx, ey, ex);
 
-  public static boolean confirmIsAllSame(int[][] map, int startNum) {
-    for (int[] ny : map) {
-      for (int y : ny) {
-        if (startNum != y) return false;
-      }
-    }
-    return true;
-  }
+			sb.append(")");
+			return;
+		} else {
+			sb.append(initV);
+		}
 
-  public static void main(String[] args) throws IOException {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	}
 
-    int N = Integer.parseInt(br.readLine());
-    int[][] map = new int[N][N];
+	public static void main(String[] args) throws IOException {
 
-    String str;
-    for (int i = 0; i < N; i++) {
-      str = br.readLine();
-      for (int j = 0; j < N; j++) {
-        map[i][j] = str.charAt(j) - '0';
-      }
-    }
+		N = Integer.parseInt(br.readLine());
 
-    String answer = solve(N, map);
-    System.out.println(answer);
-  }
+		quad = new int[N][N];
+
+		String line;
+		for (int i = 0; i < N; i++) {
+			line = br.readLine();
+			for (int j = 0; j < N; j++) {
+				quad[i][j] = line.charAt(j) - '0';
+			}
+		}
+
+		check(0, 0, N, N);
+
+		bw.write(sb.toString());
+		bw.flush();
+		// 사분할 영역을 체크하러 간다
+
+	}
+
 }
