9cd3170e837e088cf6137f9822f3dfc0e533aedd
KGYURY <71677810+kimkyury@users.noreply.github.com>
2024-10-20 22:36:03 +0900

    [Bronze II] Title: 이진수 덧셈, Time: 100 ms, Memory: 14176 KB -BaekjoonHub

--- 백준/Bronze/1252. 이진수 덧셈/이진수 덧셈.java
+++ 백준/Bronze/1252. 이진수 덧셈/이진수 덧셈.java
@@ -14,13 +14,57 @@ public class Main {
         String str1 = st.nextToken();
         String str2 = st.nextToken();
 
-        BigInteger num1 = new BigInteger(str1, 2);
-        BigInteger num2 = new BigInteger(str2, 2);
+        if (str1.length() < str2.length()) {
+            String temp = str2;
+            str2 = str1;
+            str1 = temp;
+        }
 
-        BigInteger sum =num1.add(num2);
+        String result = calcul(str1, str2, 0, 0, new StringBuilder());
 
-        bw.write(sum.toString(2));
+        int idx = 0;
+        // 1이 될 때까지 찾기
+        while (idx < result.length() && result.charAt(idx) != '1') {
+            idx++;
+        }
+
+        // 모두 0이여서 idx가 끝까지 가버림
+        if (idx == result.length()) {
+            bw.write("0");
+        } else {
+            bw.write(result.substring(idx));
+        }
+
         bw.flush();
+    }
+
+    private static String calcul(String str1, String str2, int idx, int carry, StringBuilder sb) {
+
+        if (idx == str1.length()) {
+            if (carry == 1) {
+                sb.insert(0, 1);
+            }
+            return sb.toString();
+        }
+
+        // 짧은 이진수는 볼 거 다 봤다면
+        if (idx >= str2.length()) {
+            int num = str1.charAt(str1.length() - 1 - idx) - '0';
+            if (num + carry == 2) {
+                return calcul(str1, str2, idx + 1, 1, sb.insert(0, '0'));
+            } else {
+                return calcul(str1, str2, idx + 1, 0, sb.insert(0, num + carry));
+            }
+        }
+
+        int num1 = str1.charAt(str1.length() - 1 - idx) - '0';
+        int num2 = str2.charAt(str2.length() - 1 - idx) - '0';
+        int sum = num1 + num2 + carry;
+        if (sum >= 2) {
+            return calcul(str1, str2, idx + 1, 1, sb.insert(0, sum - 2));
+        } else {
+            return calcul(str1, str2, idx + 1, 0, sb.insert(0, sum));
+        }
     }
 
 }
